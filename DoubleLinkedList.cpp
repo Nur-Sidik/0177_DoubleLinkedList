bdc71927419747e82968fd6c7c74ec2800e23a7b
Nur-Sidik <nursidik1500@gmail.com>
2025-05-15 11:26:43 +0700

    menjalankan perintah current next dan preview

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -62,3 +62,12 @@ public:
             return;
         }
         
+        newNode->next = current->next;
+        newNode->prev = current;
+
+        if (current->next != NULL)
+            current->next->prev = newNode;
+
+
+        current->next = newNode;
+    }
